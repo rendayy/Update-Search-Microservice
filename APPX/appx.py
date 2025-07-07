14fe8e44cf5cf1cd6615d2cb7f33b900b40ebf79
unknown <raihanrizky1308@gmail.com>
2025-07-07 09:59:57 +0700

    Menambahkan Search dan lain lain

--- APPX/appx.py
+++ APPX/appx.py
@@ -30,7 +30,6 @@ def ms2():
     datas = requests.get(alamatserver)
     rows = json.loads(datas.text)
     return render_template('indexms.html', rows=rows,  servermana=servermana, DB='DB-A')
-    
 
 @app.route('/ms3')
 def ms3():
@@ -47,6 +46,7 @@ def createcar(ms):
     except:
         ms = 'MS1'
         return render_template('createcar.html', servermana=ms)
+
 @app.route('/createcarsave_ms1', methods=['GET','POST'])
 def createcarsave_ms1():
     fName = request.form['carName']
@@ -99,7 +99,6 @@ def createcarsave_ms2():
 
     return redirect(url_for('ms2'))
 
-
 @app.route('/createcarsave_ms3', methods=['GET','POST'])
 def createcarsave_ms3():
     fName = request.form['carName']
@@ -126,9 +125,9 @@ def createcarsave_ms3():
 
     return redirect(url_for('ms3'))
 
-
 @app.route('/readcar/<ms>')
 def readcar(ms):
+    ms = ms.upper()
     if ms in ['MS1','MS2']:
         alamatserver = "http://localhost:5051/cars"
         datas = requests.get(alamatserver)
@@ -142,22 +141,83 @@ def readcar(ms):
         rows = json.loads(datas.text)
         return render_template('readcar.html', rows=rows, servermana=ms, DB='DB-B')
 
-@app.route('/updatecar')
-def updatecar():
-    return render_template('updatecar.html')
+@app.route('/updatecar/<ms>', methods=['GET', 'POST'])
+def updatecar(ms):
+    ms = ms.upper()
+    if request.method == 'POST':
+        car_id = request.form['carId']
 
-@app.route('/deletecar')
-def deletecar():
-    return render_template('deletecar.html')
+        datacar = {
+            "carname": request.form['carName'],
+            "carbrand": request.form['carBrand'],
+            "carmodel": request.form['carModel'],
+            "carprice": request.form['carPrice'],
+            "description": f"updated from {ms}"
+        }
 
-@app.route('/searchcar')
-def searchcar():
-    return render_template('searchcar.html')
+        datacar_json = json.dumps(datacar)
 
+        if ms == 'MS1':
+            alamatserver = f"http://localhost:5051/cars/{car_id}"
+        elif ms == 'MS2':
+            alamatserver = f"http://localhost:5052/cars/{car_id}"
+        elif ms == 'MS3':
+            alamatserver = f"http://localhost:5053/cars/{car_id}"
+        else:
+            return "Server tidak valid"
+
+        headers = {'Content-Type': 'application/json'}
+        res = requests.put(alamatserver, data=datacar_json, headers=headers)
+
+        return redirect(url_for('readcar', ms=ms))
+
+    return render_template('updatecar.html', servermana=ms)
+
+@app.route('/deletecar/<ms>', methods=['GET', 'POST'])
+def deletecar(ms):
+    ms = ms.upper()
+    if request.method == 'POST':
+        car_id = request.form['carId']
+
+        if ms == 'MS1':
+            alamatserver = f"http://localhost:5051/cars/{car_id}"
+        elif ms == 'MS2':
+            alamatserver = f"http://localhost:5052/cars/{car_id}"
+        elif ms == 'MS3':
+            alamatserver = f"http://localhost:5053/cars/{car_id}"
+        else:
+            return "Server tidak valid"
+
+        res = requests.delete(alamatserver)
+        return redirect(url_for('readcar', ms=ms))
+
+    return render_template('deletecar.html', servermana=ms)
+
+@app.route('/searchcar/<ms>', methods=['GET', 'POST'])
+def searchcar(ms):
+    hasil = []
+    ms = ms.upper()
+    if request.method == 'POST':
+        keyword = request.form['keyword'].lower()
+
+        if ms == 'MS1':
+            alamatserver = "http://localhost:5051/cars"
+        elif ms == 'MS2':
+            alamatserver = "http://localhost:5052/cars"
+        elif ms == 'MS3':
+            alamatserver = "http://localhost:5053/cars"
+        else:
+            return "Server tidak valid"
+
+        res = requests.get(alamatserver)
+        semua_data = res.json()
+
+        hasil = [row for row in semua_data if keyword in row['carname'].lower()]
+
+    return render_template('searchcar.html', hasil=hasil, servermana=ms)
 
 if __name__ == '__main__':
-    
     app.run(
         host = '0.0.0.0',
-        debug = 'True'
-        )
+        debug = True
+    )
--- SiteA/MS1/ms1.py
+++ SiteA/MS1/ms1.py
@@ -80,7 +80,50 @@ class CAR(Resource):
             })
         return jsonify(datas)
 
+class CARbyID(Resource):
+    def put(self, id):
+        parserData = reqparse.RequestParser()
+        parserData.add_argument('carname')
+        parserData.add_argument('carbrand')
+        parserData.add_argument('carmodel')
+        parserData.add_argument('carprice')
+        parserData.add_argument('description')
+        parserAmbilData = parserData.parse_args()
+
+        try:
+            mobil = TBCarsWeb.get(TBCarsWeb.id == id)
+            mobil.carname = parserAmbilData.get('carname')
+            mobil.carbrand = parserAmbilData.get('carbrand')
+            mobil.carmodel = parserAmbilData.get('carmodel')
+            mobil.carprice = parserAmbilData.get('carprice')
+            mobil.description = parserAmbilData.get('description')
+            mobil.save()
+
+            return jsonify({
+                'message': 'Data updated',
+                'data': {
+                    'id': mobil.id,
+                    'carname': mobil.carname,
+                    'carbrand': mobil.carbrand,
+                    'carmodel': mobil.carmodel,
+                    'carprice': mobil.carprice,
+                    'description': mobil.description
+                }
+            })
+        except TBCarsWeb.DoesNotExist:
+            return jsonify({'message': 'Car not found'}), 404
+
+    def delete(self, id):
+        try:
+            mobil = TBCarsWeb.get(TBCarsWeb.id == id)
+            mobil.delete_instance()
+            return jsonify({'message': f'Car with id {id} deleted.'})
+        except TBCarsWeb.DoesNotExist:
+            return jsonify({'message': 'Car not found'}), 404
+
+
 api.add_resource(CAR, '/cars/', endpoint="cars/")
+api.add_resource(CARbyID, '/cars/<int:id>', endpoint="car_by_id")
 
 
 if __name__ == '__main__':
